d24e2014f22eea1d4c7801436d80e66537fb01bc
Asger Vejen Hoedt <asgerhoedt@gmail.com>
2015-03-21 13:21:39 +0100

    Renamed Vec to Float3 to avoid nameclashes with Vec in std::vec.

--- src/main.rs
+++ src/main.rs
@@ -13,83 +13,83 @@ extern crate rand;
 use std::f64::consts::PI;
 
 #[derive(Copy, Clone)]
-struct Vec {
+struct Float3 {
     x: f64,
     y: f64,
     z: f64,
 }
 
-impl Vec {
-    fn new(x: f64, y: f64, z: f64) -> Vec {
-        Vec { x: x, y: y, z: z }
+impl Float3 {
+    fn new(x: f64, y: f64, z: f64) -> Float3 {
+        Float3 { x: x, y: y, z: z }
     }
 
-    fn zero() -> Vec { Vec { x:0.0, y:0.0, z:0.0 } }
+    fn zero() -> Float3 { Float3 { x:0.0, y:0.0, z:0.0 } }
 
-    fn dot(lhs: Vec, rhs: Vec) -> f64 {
+    fn dot(lhs: Float3, rhs: Float3) -> f64 {
         lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z
     }
 
-    fn cross(lhs: Vec, rhs: Vec) -> Vec {
-        Vec { x: lhs.y * rhs.z - lhs.z * rhs.y,
+    fn cross(lhs: Float3, rhs: Float3) -> Float3 {
+        Float3 { x: lhs.y * rhs.z - lhs.z * rhs.y,
               y: lhs.z * rhs.x - lhs.x * rhs.z,
               z: lhs.x * rhs.y - lhs.y * rhs.x }
     }
 
     fn length(self) -> f64 {
-        Vec::dot(self, self).sqrt()
+        Float3::dot(self, self).sqrt()
     }
 
-    fn normalized(self) -> Vec {
+    fn normalized(self) -> Float3 {
         let length = self.length();
-        Vec { x: self.x / length, y: self.y / length, z: self.z / length }
+        Float3 { x: self.x / length, y: self.y / length, z: self.z / length }
     }
 }
 
-impl Add for Vec {
-    type Output = Vec;
-    fn add(self, rhs: Vec) -> Vec {
-        Vec { x: self.x + rhs.x,
+impl Add for Float3 {
+    type Output = Float3;
+    fn add(self, rhs: Float3) -> Float3 {
+        Float3 { x: self.x + rhs.x,
               y: self.y + rhs.y,
               z: self.z + rhs.z }
     }
 }
 
-impl Sub for Vec {
-    type Output = Vec;
-    fn sub(self, rhs: Vec) -> Vec {
-        Vec { x: self.x - rhs.x,
+impl Sub for Float3 {
+    type Output = Float3;
+    fn sub(self, rhs: Float3) -> Float3 {
+        Float3 { x: self.x - rhs.x,
               y: self.y - rhs.y,
               z: self.z - rhs.z }
     }
 }
 
-impl Mul for Vec {
-    type Output = Vec;
-    fn mul(self, rhs: Vec) -> Vec {
-        Vec { x: self.x * rhs.x,
+impl Mul for Float3 {
+    type Output = Float3;
+    fn mul(self, rhs: Float3) -> Float3 {
+        Float3 { x: self.x * rhs.x,
               y: self.y * rhs.y,
               z: self.z * rhs.z }
     }
 }
 
-impl Mul<f64> for Vec {
-    type Output = Vec;
-    fn mul(self, rhs: f64) -> Vec {
-        Vec { x: self.x * rhs,
+impl Mul<f64> for Float3 {
+    type Output = Float3;
+    fn mul(self, rhs: f64) -> Float3 {
+        Float3 { x: self.x * rhs,
               y: self.y * rhs,
               z: self.z * rhs }
     }
 }
 
-fn luma(color: Vec) -> f64 {
+fn luma(color: Float3) -> f64 {
     0.299 * color.x + 0.587 * color.y + 0.114 * color.z
 }
 
 #[derive(Copy, Clone)]
 struct Ray {
-    origin: Vec,
-    direction: Vec,
+    origin: Float3,
+    direction: Float3,
 }
 
 #[derive(Copy, Clone)]
@@ -98,14 +98,14 @@ enum BSDF { Diffuse, Mirror, Glass }
 #[derive(Copy, Clone)]
 struct Sphere {
     radius: f64,
-    position: Vec,
-    emission: Vec,
-    albedo: Vec,
+    position: Float3,
+    emission: Float3,
+    albedo: Float3,
     bsdf: BSDF,
 }
 
 impl Sphere {
-    fn new(radius: f64, position: Vec, emission: Vec, albedo: Vec, bsdf: BSDF) -> Sphere{
+    fn new(radius: f64, position: Float3, emission: Float3, albedo: Float3, bsdf: BSDF) -> Sphere{
         Sphere { radius: radius, position: position, emission: emission, albedo: albedo, bsdf: bsdf } 
     }
 }
@@ -113,9 +113,9 @@ impl Sphere {
 // returns distance, infinity if no hit.
 fn intersect(ray: Ray, sphere: &Sphere) -> f64{
     // Solve t^2*d.d + 2*t*(o-p).d + (o-p).(o-p)-R^2 = 0
-    let op: Vec = sphere.position - ray.origin;
-    let b: f64 = Vec::dot(op, ray.direction);
-    let det_sqrd: f64 = b * b - Vec::dot(op,op) + sphere.radius * sphere.radius;
+    let op: Float3 = sphere.position - ray.origin;
+    let b: f64 = Float3::dot(op, ray.direction);
+    let det_sqrd: f64 = b * b - Float3::dot(op,op) + sphere.radius * sphere.radius;
     if det_sqrd < 0.0 {
         return Float::infinity();
     }
@@ -161,14 +161,14 @@ fn intersect_scene(ray: Ray, scene: &[Sphere]) -> Option<(&Sphere, f64)> {
     }
 }
 
-fn radiance_estimation(ray: Ray, scene: &[Sphere], depth: i32) -> Vec {
+fn radiance_estimation(ray: Ray, scene: &[Sphere], depth: i32) -> Float3 {
     let intersection: Option<(&Sphere, f64)> = intersect_scene(ray, scene);
     match intersection {
-        None => Vec::zero(),
+        None => Float3::zero(),
         Some ((sphere, t)) => {
             let position = ray.origin + ray.direction * t;
             let hard_normal = (position - sphere.position).normalized();
-            let forward_normal = if Vec::dot(hard_normal, ray.direction) < 0.0 { hard_normal } else { hard_normal * -1.0 };
+            let forward_normal = if Float3::dot(hard_normal, ray.direction) < 0.0 { hard_normal } else { hard_normal * -1.0 };
             let mut f = sphere.albedo;
             if depth > 3 {
                 if rand::random::<f64>() < luma(f) && depth < 100 {
@@ -185,27 +185,27 @@ fn radiance_estimation(ray: Ray, scene: &[Sphere], depth: i32) -> Vec {
                     let r1 = 2.0 * PI * rand::random::<f64>();
                     let r2 = rand::random::<f64>();
                     let r2s = r2.sqrt();
-                    let wup = if forward_normal.x.abs() > 0.1 { Vec::new(0.0, 1.0, 0.0) } else { Vec::new(1.0, 0.0, 0.0) };
-                    let tangent = Vec::cross(forward_normal, wup).normalized();
-                    let bitangent = Vec::cross(forward_normal, tangent).normalized(); // Normalize due to precision (although probably not needed when using f64:))
+                    let wup = if forward_normal.x.abs() > 0.1 { Float3::new(0.0, 1.0, 0.0) } else { Float3::new(1.0, 0.0, 0.0) };
+                    let tangent = Float3::cross(forward_normal, wup).normalized();
+                    let bitangent = Float3::cross(forward_normal, tangent).normalized(); // Normalize due to precision (although probably not needed when using f64:))
                     let next_direction = tangent * r1.cos() * r2s + bitangent * r1.sin() * r2s + forward_normal * (1.0 - r2).sqrt();
                     radiance_estimation(Ray { origin: position, direction: next_direction.normalized() }, scene, depth+1)
                 },
 
                 BSDF::Mirror => {
-                    let reflected = ray.direction - forward_normal * 2.0 * Vec::dot(forward_normal, ray.direction);
+                    let reflected = ray.direction - forward_normal * 2.0 * Float3::dot(forward_normal, ray.direction);
                     radiance_estimation(Ray { origin: position, direction: reflected }, scene, depth+1)
                 },
 
                 BSDF::Glass => {
-                    let reflected = ray.direction - forward_normal * 2.0 * Vec::dot(forward_normal, ray.direction);                    
+                    let reflected = ray.direction - forward_normal * 2.0 * Float3::dot(forward_normal, ray.direction);                    
                     let reflection_ray = Ray { origin: position, direction: reflected };
                     // Compute fresnel.
-                    let into = Vec::dot(hard_normal, forward_normal) > 0.0;
+                    let into = Float3::dot(hard_normal, forward_normal) > 0.0;
                     let nc = 1.0;
                     let nt = 1.5;
                     let nnt = if into { nc / nt } else { nt / nc };
-                    let ddn = Vec::dot(ray.direction, forward_normal);
+                    let ddn = Float3::dot(ray.direction, forward_normal);
                     let cos2t = 1.0 - nnt * nnt * (1.0 - ddn * ddn);
                     if cos2t < 0.0 { // Total internal reflection
                         radiance_estimation(reflection_ray, scene, depth+1)
@@ -215,7 +215,7 @@ fn radiance_estimation(ray: Ray, scene: &[Sphere], depth: i32) -> Vec {
                         let a = nt-nc;
                         let b = nt + nc;
                         let base_reflectance = a * a / (b * b);
-                        let c = 1.0 - if into { -ddn } else { Vec::dot(transmitted_dir, hard_normal) };
+                        let c = 1.0 - if into { -ddn } else { Float3::dot(transmitted_dir, hard_normal) };
                         let reflectance = base_reflectance + (1.0 - base_reflectance) * c * c * c * c * c;
                         let transmittance = 1.0 - reflectance;
                         let rr_propability = 0.25 + 0.5 * reflectance;
@@ -241,15 +241,15 @@ fn radiance_estimation(ray: Ray, scene: &[Sphere], depth: i32) -> Vec {
 
 fn main() {
     
-    let scene = [Sphere::new(1e5, Vec::new(1e5+1.0,40.8,81.6),      Vec::zero(),              Vec::new(0.75,0.25,0.25),    BSDF::Diffuse),  //Left
-                 Sphere::new(1e5, Vec::new(-1e5+99.0,40.8,81.6),    Vec::zero(),              Vec::new(0.25,0.25,0.75),    BSDF::Diffuse),  //Right
-                 Sphere::new(1e5, Vec::new(50.0,40.8, 1e5),         Vec::zero(),              Vec::new(0.75,0.75,0.75),    BSDF::Diffuse),  //Back
-                 Sphere::new(1e5, Vec::new(50.0,40.8,-1e5+170.0),   Vec::zero(),              Vec::zero(),                 BSDF::Diffuse),  //Front
-                 Sphere::new(1e5, Vec::new(50.0, 1e5, 81.6),        Vec::zero(),              Vec::new(0.75,0.75,0.75),    BSDF::Diffuse),  //Bottom
-                 Sphere::new(1e5, Vec::new(50.0,-1e5+81.6,81.6),    Vec::zero(),              Vec::new(0.75,0.75,0.75),    BSDF::Diffuse),  //Top
-                 Sphere::new(16.5,Vec::new(27.0,16.5,47.0),         Vec::zero(),              Vec::new(0.999,0.999,0.999), BSDF::Mirror),   //Mirror
-                 Sphere::new(16.5,Vec::new(73.0,16.5,78.0),         Vec::zero(),              Vec::new(0.999,0.999,0.999), BSDF::Glass),    //Glass
-                 Sphere::new(600.0, Vec::new(50.0,681.6-0.27,81.6), Vec::new(12.0,12.0,12.0), Vec::zero(),                 BSDF::Diffuse)]; //Light
+    let scene = [Sphere::new(1e5, Float3::new(1e5+1.0,40.8,81.6),      Float3::zero(),              Float3::new(0.75,0.25,0.25),    BSDF::Diffuse),  //Left
+                 Sphere::new(1e5, Float3::new(-1e5+99.0,40.8,81.6),    Float3::zero(),              Float3::new(0.25,0.25,0.75),    BSDF::Diffuse),  //Right
+                 Sphere::new(1e5, Float3::new(50.0,40.8, 1e5),         Float3::zero(),              Float3::new(0.75,0.75,0.75),    BSDF::Diffuse),  //Back
+                 Sphere::new(1e5, Float3::new(50.0,40.8,-1e5+170.0),   Float3::zero(),              Float3::zero(),                 BSDF::Diffuse),  //Front
+                 Sphere::new(1e5, Float3::new(50.0, 1e5, 81.6),        Float3::zero(),              Float3::new(0.75,0.75,0.75),    BSDF::Diffuse),  //Bottom
+                 Sphere::new(1e5, Float3::new(50.0,-1e5+81.6,81.6),    Float3::zero(),              Float3::new(0.75,0.75,0.75),    BSDF::Diffuse),  //Top
+                 Sphere::new(16.5,Float3::new(27.0,16.5,47.0),         Float3::zero(),              Float3::new(0.999,0.999,0.999), BSDF::Mirror),   //Mirror
+                 Sphere::new(16.5,Float3::new(73.0,16.5,78.0),         Float3::zero(),              Float3::new(0.999,0.999,0.999), BSDF::Glass),    //Glass
+                 Sphere::new(600.0, Float3::new(50.0,681.6-0.27,81.6), Float3::new(12.0,12.0,12.0), Float3::zero(),                 BSDF::Diffuse)]; //Light
 
     const WIDTH: usize = 512;
     const HEIGHT: usize = 512;
@@ -263,12 +263,12 @@ fn main() {
                       None => 1
                   };
 
-    let cam = Ray { origin: Vec::new(50.0, 52.0, 295.6), direction: Vec::new(0.0, -0.042612, -1.0).normalized() };
-    let cx = Vec { x: WIDTH as f64 * 0.5135 / HEIGHT as f64, y: 0.0, z: 0.0 } ;
-    let cy = Vec::cross(cx, cam.direction).normalized() * 0.5135;
+    let cam = Ray { origin: Float3::new(50.0, 52.0, 295.6), direction: Float3::new(0.0, -0.042612, -1.0).normalized() };
+    let cx = Float3 { x: WIDTH as f64 * 0.5135 / HEIGHT as f64, y: 0.0, z: 0.0 } ;
+    let cy = Float3::cross(cx, cam.direction).normalized() * 0.5135;
     
     // Fill backbuffer multithreaded
-    let mut backbuffer = std::vec::from_elem(Vec::zero(), WIDTH * HEIGHT);
+    let mut backbuffer = std::vec::from_elem(Float3::zero(), WIDTH * HEIGHT);
     {
         let outer_chunks = 100;
         let outer_chunk_size = ceil_divide(WIDTH * HEIGHT, outer_chunks);
@@ -281,15 +281,15 @@ fn main() {
             let inner_chunk_size = ceil_divide(outer_chunk_size, inner_chunks);
             
             // Create and launch threads. Automatically joins when going out of scope.
-            let mut threads = std::vec::Vec::with_capacity(inner_chunks);
+            let mut threadscope = std::vec::Vec::with_capacity(inner_chunks);
             for (inner_chunk_index, inner_chunk) in outer_chunk.chunks_mut(inner_chunk_size).enumerate() {
-                threads.push(thread::scoped(move || {
+                threadscope.push(thread::scoped(move || {
                     for i in 0..inner_chunk.len() {
                         let pixel_index = i + inner_chunk_index * inner_chunk_size + outer_chunk_index * outer_chunk_size;
                         let x = pixel_index % WIDTH;
                         let y = HEIGHT - pixel_index / WIDTH - 1;
                         
-                        let mut radiance = Vec::zero();
+                        let mut radiance = Float3::zero();
                         // Sample 2x2 subpixels.
                         for sy in 0..2 {
                             for sx in 0..2 {
